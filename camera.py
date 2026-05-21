c61a9bf20b5b9b5df143f8692ce35fad67f5cb9e
Melike Abdurrahmanoglu <melikemmm125@gmail.com>
2026-05-21 20:06:53 +0300

    güncellendi

--- camera.py
+++ camera.py
@@ -1,32 +1,49 @@
 # camera.py
 
+from picamera2 import Picamera2
 import cv2
-from config import CAMERA_INDEX, FRAME_WIDTH, FRAME_HEIGHT
+
+from config import FRAME_WIDTH, FRAME_HEIGHT
+
 
 class CameraSystem:
 
     def __init__(self):
-        self.cap = cv2.VideoCapture(CAMERA_INDEX)
 
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+        self.picam2 = Picamera2()
+
+        config = self.picam2.create_preview_configuration(
+            main={
+                "size": (FRAME_WIDTH, FRAME_HEIGHT),
+                "format": "RGB888"
+            }
+        )
+
+        self.picam2.configure(config)
+
+        self.picam2.start()
+
+        self.opened = True
+
+        print("CSI kamera baslatildi.")
 
     def is_opened(self):
-        return self.cap.isOpened()
+        return self.opened
 
     def read_frame(self):
-        ret, frame = self.cap.read()
 
-        if not ret:
-            return None
+        frame = self.picam2.capture_array()
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
         return frame
 
+    def release(self):
+
+        self.picam2.stop()
+
     def get_width(self):
-        return int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        return FRAME_WIDTH
 
     def get_height(self):
-        return int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    def release(self):
-        self.cap.release()
+        return FRAME_HEIGHT
